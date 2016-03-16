6e032e1aa2828d8f684f31dd5af4966c745d04db
Jean D'Amore <jdamore@thoughtworks.com>
2016-03-16 22:49:24 +1000

    Added specs

---
+++ spec/word_service_spec.rb
@@ -0,0 +1,14 @@
+require_relative 'spec_helper'
+
+describe 'WordService' do
+
+  before do
+  end
+ 
+  it "should have an error endpoint" do
+  end
+ 
+  it "shoud have a word endpoint" do
+  end
+
+end
